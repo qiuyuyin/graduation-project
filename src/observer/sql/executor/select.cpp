7762c3e5a28779fdc699270216f0b90416a9dfa3
Joenhle <820863776@qq.com>
2022-10-31 20:08:43 +0800

    feat:支持update-select的子查询结构

--- src/observer/sql/executor/select.cpp
+++ src/observer/sql/executor/select.cpp
@@ -11,8 +11,11 @@
 #include "sql/stmt/typecaster.h"
 #include "util/util.h"
 #include "sql/operator/join_operator.h"
+#include "sql/operator/operator.h"
 
 using namespace std;
+
+
 
 RC ExecuteStage::do_select(SQLStageEvent *sql_event)
 {
@@ -157,6 +160,45 @@ RC ExecuteStage::do_select(SQLStageEvent *sql_event)
     LOG_WARN("failed to open operator");
     return rc;
   }
+
+  //子查询
+  if (sql_event->is_sub_query()) {
+    vector<VTuple*> sub_select_res;
+    while ((rc = oper->next()) == SUCCESS) {
+      Tuple* tuple = oper->current_tuple();
+      if (nullptr == tuple) {
+        rc = RC::INTERNAL;
+        LOG_WARN("failed to get current record. rc=%s", strrc(rc));
+        break;
+      }
+      VTuple *temp = new VTuple;
+      switch (tuple->get_tuple_type()) {
+        case TupleType::ROW:{
+          temp->append_row_tuple(*(RowTuple*)(tuple));
+          break;
+        }
+        case TupleType::V:{
+          temp->merge(*tuple,*temp);
+          break;
+        }
+        default:{
+          LOG_WARN("UNKNOWN TUPLE_TYPE");
+          break;
+        }
+      }
+      sub_select_res.push_back(temp);
+    }
+    if (rc != RC::RECORD_EOF) {
+      LOG_WARN("something wrong while iterate operator. rc=%s", strrc(rc));
+      oper->close();
+      return rc;
+    }
+    rc = oper->close();
+    sql_event->set_sub_query_res(sub_select_res);
+    return SUCCESS;
+  }
+
+  //正常查询
   print_tuple_header(ss, *oper);
   while ((rc = oper->next()) == RC::SUCCESS) {
     Tuple * tuple = oper->current_tuple();
@@ -176,99 +218,7 @@ RC ExecuteStage::do_select(SQLStageEvent *sql_event)
   }
   session_event->set_response(ss.str());
   return RC::SUCCESS;
-  /*
-sql_event->session_event()->set_response("B");
-return RC::SUCCESS;
-   */
-}
 
-//RC ExecuteStage::do_select(SQLStageEvent *sql_event)
-//{
-// return do_select_v2(sql_event);
-//  SelectStmt *select_stmt = (SelectStmt *)(sql_event->stmt());
-//  SessionEvent *session_event = sql_event->session_event();
-//  RC rc = RC::SUCCESS;
-//  stringstream ss;
-//
-//  if(select_stmt->tables().size()!=1){
-//    std::unordered_map<std::string,Operator *> table2operator;
-//  }else{
-//    todo filter_unit的expr包含计算和聚合的时候直接不走索引，注意赛选
-//    Operator *scan_oper = try_to_create_index_scan_operator(select_stmt->filter_stmt());
-//    if (nullptr == scan_oper) {
-//      scan_oper = new TableScanOperator(select_stmt->tables()[0]);
-//    }
-//
-//    Operator *scan_oper = new TableScanOperator(select_stmt->tables()[0]);
-//
-//    DEFER([&] () {delete scan_oper;});
-//    PredicateOperator pred_oper(select_stmt->filter_stmt()->filter_units());
-//    pred_oper.add_child(scan_oper);
-//
-//    AggregateOperator *agg_oper = nullptr;
-//    SortOperator *sort_oper = nullptr;
-//    if(select_stmt->aggregate_fields().size()!=0 ){
-//      vector<shared_ptr<TupleCellSpec>> gb_fields;
-//      for(auto gb_field : select_stmt->groupby_fields()){
-//        gb_fields.push_back(gb_field);
-//      }
-//      agg_oper = new AggregateOperator(select_stmt->aggregate_fields(),gb_fields);
-//      agg_oper->add_child(&pred_oper);
-//    }
-//    if(select_stmt->orderby_fields().size()!=0){
-//      sort_oper = new SortOperator(select_stmt->orderby_fields());
-//      if(agg_oper != nullptr){
-//        sort_oper->add_child(agg_oper);
-//      }else{
-//        sort_oper->add_child(&pred_oper);
-//      }
-//    }
-//
-//    ProjectOperator project_oper;
-//    if(sort_oper == nullptr){
-//      if(agg_oper == nullptr){
-//        project_oper.add_child(&pred_oper);
-//      }else{
-//        project_oper.add_child(agg_oper);
-//      }
-//    }else{
-//      project_oper.add_child(sort_oper);
-//    }
-//    //project_oper.add_child(&pred_oper);
-//
-//    for (auto tupleCell : select_stmt->query_fields()) {
-//      project_oper.add_projection(tupleCell);
-//    }
-//    rc = project_oper.open();
-//    if (rc != RC::SUCCESS) {
-//      LOG_WARN("failed to open operator");
-//      return rc;
-//    }
-//
-//    print_tuple_header(ss, project_oper);
-//    while ((rc = project_oper.next()) == RC::SUCCESS) {
-//      // get current record
-//      // write to response
-//      Tuple * tuple = project_oper.current_tuple();
-//      if (nullptr == tuple) {
-//        rc = RC::INTERNAL;
-//        LOG_WARN("failed to get current record. rc=%s", strrc(rc));
-//        break;
-//      }
-//
-//      tuple_to_string(ss, *tuple);
-//      ss << std::endl;
-//    }
-//
-//    if (rc != RC::RECORD_EOF) {
-//      LOG_WARN("something wrong while iterate operator. rc=%s", strrc(rc));
-//      project_oper.close();
-//    } else {
-//      rc = project_oper.close();
-//    }
-//  }
-//  session_event->set_response(ss.str());
-//  return rc;
-//}
+}
 
 
